c7c26a484456d1f944d4d6331e9857c431cc2a19
Muhammad-Jibril <jbashir52@gmail.com>
2021-06-09 00:28:10 +0800

    Add log clean

--- index.js
+++ index.js
@@ -1,75 +1,83 @@
-
 var { benchBranch, benchmarkRuntime } = require("./bench");
 
 module.exports = app => {
-  app.log(`base branch: ${process.env.BASE_BRANCH}`);
+    app.log(`base branch: ${process.env.BASE_BRANCH}`);
 
-  app.on('issue_comment', async context => {
-    let commentText = context.payload.comment.body;
-    const triggerCommand = "/bench";
-    if (!commentText.startsWith(triggerCommand)) {
-      return;
-    }
+    app.on('issue_comment', async context => {
+        let commentText = context.payload.comment.body;
+        const triggerCommand = "/bench";
+        if (!commentText.startsWith(triggerCommand)) {
+            return;
+        }
 
-    // Capture `<action>` in `/bench <action> <extra>`
-    let [action, ...rest] = commentText.slice(triggerCommand.length).trim().split(" ");
-    // Rest is `<extra>`
-    let extra = rest.join(" ").trim();
+        // Capture `<action>` in `/bench <action> <extra>`
+        let [action, ...rest] = commentText.slice(triggerCommand.length).trim().split(" ");
+        // Rest is `<extra>`
+        let extra = rest.join(" ").trim();
 
-    if (action === "clean") {
-      shell.exec("rm -rf git", { silent: false });
-      return;
-    }
+        if (action === "clean") {
+            app.log('execute clean command');
+            shell.exec("rm -rf git", { silent: false });
+            const issueComment = context.issue({ body: `Clean done` });
+            await context.github.issues.createComment(issueComment);
+            return;
+        }
 
-    const repo = context.payload.repository.name;
-    const owner = context.payload.repository.owner.login;
-    const pull_number = context.payload.issue.number;
+        const repo = context.payload.repository.name;
+        const owner = context.payload.repository.owner.login;
+        const pull_number = context.payload.issue.number;
 
-    let pr = await context.github.pulls.get({ owner, repo, pull_number });
-    const branchName = pr.data.head.ref;
-    app.log(`branch: ${branchName}`);
-    const issueComment = context.issue({ body: `Starting benchmark for branch: ${branchName} (vs ${process.env.BASE_BRANCH})\n\n Comment will be updated.` });
-    const issue_comment = await context.github.issues.createComment(issueComment);
-    const comment_id = issue_comment.data.id;
+        let pr = await context.github.pulls.get({ owner, repo, pull_number });
+        const branchName = pr.data.head.ref;
+        app.log(`branch: ${branchName}`);
+        const issueComment = context.issue({ body: `Starting benchmark for branch: ${branchName} (vs ${process.env.BASE_BRANCH})\n\n Comment will be updated.` });
+        const issue_comment = await context.github.issues.createComment(issueComment);
+        const comment_id = issue_comment.data.id;
 
-    let config = {
-      owner: owner,
-      repo: repo,
-      branch: branchName,
-      baseBranch: process.env.BASE_BRANCH,
-      id: action,
-      pushToken: process.env.PUSH_TOKEN,
-      extra: extra,
-    }
+        let config = {
+            owner: owner,
+            repo: repo,
+            branch: branchName,
+            baseBranch: process.env.BASE_BRANCH,
+            id: action,
+            pushToken: process.env.PUSH_TOKEN,
+            extra: extra,
+        }
 
-    let report;
-    if (action == "runtime") {
-      report = await benchmarkRuntime(app, config)
-    } else {
-      report = await benchBranch(app, config)
-    };
+        let report;
+        if (action == "runtime") {
+            report = await benchmarkRuntime(app, config)
+        } else {
+            report = await benchBranch(app, config)
+        };
 
-    if (report.error) {
-      app.log(`error: ${report.stderr}`)
-      if (report.step != "merge") {
-        context.github.issues.updateComment({
-          owner, repo, comment_id,
-          body: `Error running benchmark: **${branchName}**\n\n<details><summary>stdout</summary>${report.stderr}</details>`,
-        });
-      } else {
-        context.github.issues.updateComment({
-          owner, repo, comment_id,
-          body: `Error running benchmark: **${branchName}**\n\nMerge conflict merging branch to master!`,
-        });
-      }
-    } else {
-      app.log(`report: ${report}`);
-      context.github.issues.updateComment({
-        owner, repo, comment_id,
-        body: `Finished benchmark for branch: **${branchName}**\n\n${report}`,
-      });
-    }
+        if (report.error) {
+            app.log(`error: ${report.stderr}`)
+            if (report.step != "merge") {
+                context.github.issues.updateComment({
+                    owner,
+                    repo,
+                    comment_id,
+                    body: `Error running benchmark: **${branchName}**\n\n<details><summary>stdout</summary>${report.stderr}</details>`,
+                });
+            } else {
+                context.github.issues.updateComment({
+                    owner,
+                    repo,
+                    comment_id,
+                    body: `Error running benchmark: **${branchName}**\n\nMerge conflict merging branch to master!`,
+                });
+            }
+        } else {
+            app.log(`report: ${report}`);
+            context.github.issues.updateComment({
+                owner,
+                repo,
+                comment_id,
+                body: `Finished benchmark for branch: **${branchName}**\n\n${report}`,
+            });
+        }
 
-    return;
-  })
+        return;
+    })
 }
